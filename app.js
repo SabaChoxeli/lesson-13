b5989617732d19ecf9cf03232b28397fbb265be2
SabaChoxeli <sabachoxeli13@gmail.com>
2022-06-21 13:36:33 +0400

    done javascipt

---
+++ app.js
@@ -0,0 +1,27 @@
+let userName = "Saba";
+let userEmail = "Sabachoxeli13@gmail.com";
+let useCity = "Tbilisi";
+const birthYear = 2001;
+
+const myName = "Saba Chokheli";
+let myHobby = "Gaming and Swimming";
+const yearOfBirth = 2001;
+let currentYear = 2022;
+
+let firstSentence =
+  "My name is " +
+  myName +
+  ", " +
+  "I am " +
+  (currentYear - yearOfBirth) +
+  " years old and my hobby is " +
+  myHobby +
+  ".";
+
+console.log(firstSentence);
+
+let secondSentence = `My name is ${myName}, I am ${
+  currentYear - yearOfBirth
+} years old and my hobby is ${myHobby}.`;
+
+console.log(secondSentence);
